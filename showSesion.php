b3149a0702bbba229b408654b160ffd752a7d521
CBARRIUSO <CBARRIUSO@192.168.21.104>
2018-05-08 17:19:16 +0200

    sw

--- showSesion.php
+++ showSesion.php
@@ -61,6 +61,77 @@ if (!isset($_GET['code'])){
 
 <body class="text-center">
 
+	<header>
+		<div class="collapse bg-dark" id="navbarHeader">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8 col-md-7 py-4">
+						<h4 class="text-white">About</h4>
+						<p class="text-muted">FilMolin Cinema, es el nuevo cine con un
+							servicio unico que nos diferencia de otros empresas, ya que ahora
+							no solo podras reservar tu entrada, si no que podras reservar las
+							palomitas, las bebidas, las golosinas, etc. Sin tener que esperar
+							colas en la tienda. Los creadores y dueños de este cine son Pablo
+							Molina y Adrián Lobato.</p>
+					</div>
+					<div class="col-sm-4 offset-md-1 py-4">
+						<h4 class="text-white">Contactanos</h4>
+						<ul class="list-unstyled">
+							<li><a href="#" class="text-white">Siguenos en Twitter</a></li>
+							<li><a href="#" class="text-white">Siguenos en Facebook</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="navbar navbar-dark bg-dark box-shadow"
+			style="align-items: initial">
+			<div class="container d-flex justify-content-between">
+				<div class="container">
+				<?php
+    
+    if (isset($_SESSION['login'])) {
+        $login = $_SESSION['login'];
+        if ($login == 1) {
+            ?>
+					<a href="./administration/cuenta.php"><span id="icon"
+						style="float: right; width: 150px; clear: right;"
+						class="glyphicon glyphicon-user"><?php echo $_SESSION['usuario']; ?></span></a>
+
+		<?php }else{?>
+					
+					 <a href="./login/login.php" class="btn btn-primary btn-lg active"
+						role="button" aria-pressed="true"
+						style="float: right; clear: right;">Inicia sesión</a>
+					
+					<?php
+        }
+    } else {
+        ?>
+    
+    <a href="./login/login.php" class="btn btn-primary btn-lg active"
+						role="button" aria-pressed="true"
+						style="float: right; clear: right;">Inicia sesión</a>
+    <?php
+    }
+    ?>
+    
+				
+				
+				</div>
+				<a href="#" class="navbar-brand d-flex align-items-center"> <img
+					src="./img/icon.png" width="50px" height="50px">
+					<h1 style="font-size: 100px">FilMolin Cinema</h1>
+				</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse"
+					data-target="#navbarHeader" aria-controls="navbarHeader"
+					aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+			</div>
+		</div>
+
+	</header>
 	<?php 
 	
 	$resultado = $conexion->query("SELECT * FROM peliculas".$where);
